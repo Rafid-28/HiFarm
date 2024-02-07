c15c3924c5c6b77aa98fe87fc3e1289666634af3
Muhammad Rafid <96788115+Rafid-28@users.noreply.github.com>
2024-02-07 10:13:16 +0700

    anyar neh wkwk

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:hi_farm/style/app_style.dart';
-
-import 'components/bottom_bar.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hi_farm/screens/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +8,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'HiFarm',
-      theme: AppStyle.theme,
-      home: const BottomBar(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+        textTheme: GoogleFonts.mulishTextTheme(),
+      ),
+      home: const MainScreen(),
     );
   }
 }
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:hi_farm/constants.dart';
+import 'package:hi_farm/models/product.dart';
+import 'package:hi_farm/widgets/categories.dart';
+import 'package:hi_farm/widgets/home_appbar.dart';
+import 'package:hi_farm/widgets/home_slider.dart';
+import 'package:hi_farm/widgets/product_card.dart';
+import 'package:hi_farm/widgets/search_field.dart';
+import 'package:ionicons/ionicons.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentSlide = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kscaffoldColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HomeAppBar(),
+                const SizedBox(height: 20),
+                const SearchField(),
+                const SizedBox(height: 20),
+                HomeSlider(
+                  onChange: (value) {
+                    setState(() {
+                      currentSlide = value;
+                    });
+                  },
+                  currentSlide: currentSlide,
+                ),
+                const SizedBox(height: 20),
+                const Categories(),
+                const SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Special For You",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("See all"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(product: products[index]);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
